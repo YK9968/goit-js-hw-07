2d6efb3b9c32c7f28e2126997cafbb2e7c1db949
YK9968 <robka4an@gmail.com>
2024-01-25 10:00:47 +0200

    coplatd 3,4 task

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const inputEl = document.querySelector("#name-input");
+const spanEL = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (event) => {
+  if (event.target.value.trim() !== "") {
+    spanEL.textContent = event.target.value.trim();
+  } else {
+    spanEL.textContent = "Anonymous";
+  }
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const formLoginEL = document.querySelector(".login-form");
+
+function checkLogPas(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  }
+  const data = {
+    Email: `${email}`,
+    Password: `${password}`,
+  };
+  console.log(data);
+  formLoginEL.reset();
+}
+
+formLoginEL.addEventListener("submit", checkLogPas);
