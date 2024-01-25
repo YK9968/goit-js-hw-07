319c5dc9482f9d5818c5f5d06f126ecca32e0601
YK9968 <robka4an@gmail.com>
2024-01-25 15:25:22 +0200

    completed 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,21 @@
 "use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButtonEL = document.querySelector(".change-color");
+const colorSpanEL = document.querySelector(".color");
+const changeBodyColorEL = document.querySelector("body");
+
+function changeColor() {
+  const newColor = getRandomHexColor();
+  changeBodyColorEL.style.backgroundColor = newColor;
+  colorSpanEL.textContent = newColor;
+
+  console.log(changeBodyColorEL);
+}
+
+changeColorButtonEL.addEventListener("click", changeColor);
--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const addBoxes = document.querySelector("#boxes");
+const inputAdd = document.querySelector("input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+
+console.log(addBoxes);
+console.log(inputAdd);
+console.log(createButton);
+console.log(destroyButton);
+
+const value = inputAdd.addEventListener("input", (event) => {
+  event.target.value;
+  if (event.target.value >= 1 && event.target.value <= 100) {
+    console.log(event.target.value);
+  } else {
+    console.log(0);
+  }
+});
